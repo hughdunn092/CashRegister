17dbae762d5a64fe13e0e6f6f6d9ac7dbb9b60a8
hughdunn092 <hughdunn092@ed.amdsb.ca>
2023-02-24 13:55:56 -0500

    Add project files.

---
+++ CashRegister/Form1.Designer.cs
@@ -0,0 +1,360 @@
+﻿namespace CashRegister
+{
+    partial class pubBackground
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(pubBackground));
+            this.foodLabel1 = new System.Windows.Forms.Label();
+            this.foodLabel2 = new System.Windows.Forms.Label();
+            this.foodLabel3 = new System.Windows.Forms.Label();
+            this.priceBox1 = new System.Windows.Forms.TextBox();
+            this.priceBox2 = new System.Windows.Forms.TextBox();
+            this.priceBox3 = new System.Windows.Forms.TextBox();
+            this.totalButton = new System.Windows.Forms.Button();
+            this.subTotal = new System.Windows.Forms.Label();
+            this.tax = new System.Windows.Forms.Label();
+            this.total = new System.Windows.Forms.Label();
+            this.tenderedLabel = new System.Windows.Forms.Label();
+            this.tenderBox = new System.Windows.Forms.TextBox();
+            this.changeButton = new System.Windows.Forms.Button();
+            this.changeLabel = new System.Windows.Forms.Label();
+            this.receiptButton = new System.Windows.Forms.Button();
+            this.stPrice = new System.Windows.Forms.Label();
+            this.taPrice = new System.Windows.Forms.Label();
+            this.toPrice = new System.Windows.Forms.Label();
+            this.newOrder = new System.Windows.Forms.Button();
+            this.caPrice = new System.Windows.Forms.Label();
+            this.lineLabel = new System.Windows.Forms.Label();
+            this.paddysSign = new System.Windows.Forms.PictureBox();
+            this.receiptPaper = new System.Windows.Forms.Label();
+            this.menuPicture = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.paddysSign)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.menuPicture)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // foodLabel1
+            // 
+            this.foodLabel1.AutoSize = true;
+            this.foodLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.foodLabel1.Location = new System.Drawing.Point(60, 217);
+            this.foodLabel1.Name = "foodLabel1";
+            this.foodLabel1.Size = new System.Drawing.Size(206, 25);
+            this.foodLabel1.TabIndex = 0;
+            this.foodLabel1.Text = "Servings of Milk Steak";
+            this.foodLabel1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // foodLabel2
+            // 
+            this.foodLabel2.AutoSize = true;
+            this.foodLabel2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.foodLabel2.Location = new System.Drawing.Point(59, 263);
+            this.foodLabel2.Name = "foodLabel2";
+            this.foodLabel2.Size = new System.Drawing.Size(177, 25);
+            this.foodLabel2.TabIndex = 2;
+            this.foodLabel2.Text = "Slices of Rum Ham";
+            this.foodLabel2.Click += new System.EventHandler(this.foodLabel2_Click);
+            // 
+            // foodLabel3
+            // 
+            this.foodLabel3.AutoSize = true;
+            this.foodLabel3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.foodLabel3.Location = new System.Drawing.Point(59, 304);
+            this.foodLabel3.Name = "foodLabel3";
+            this.foodLabel3.Size = new System.Drawing.Size(168, 25);
+            this.foodLabel3.TabIndex = 3;
+            this.foodLabel3.Text = "Cups of Fight Milk";
+            // 
+            // priceBox1
+            // 
+            this.priceBox1.Location = new System.Drawing.Point(309, 219);
+            this.priceBox1.Name = "priceBox1";
+            this.priceBox1.Size = new System.Drawing.Size(100, 26);
+            this.priceBox1.TabIndex = 4;
+            // 
+            // priceBox2
+            // 
+            this.priceBox2.Location = new System.Drawing.Point(309, 264);
+            this.priceBox2.Name = "priceBox2";
+            this.priceBox2.Size = new System.Drawing.Size(100, 26);
+            this.priceBox2.TabIndex = 5;
+            // 
+            // priceBox3
+            // 
+            this.priceBox3.Location = new System.Drawing.Point(309, 306);
+            this.priceBox3.Name = "priceBox3";
+            this.priceBox3.Size = new System.Drawing.Size(100, 26);
+            this.priceBox3.TabIndex = 6;
+            // 
+            // totalButton
+            // 
+            this.totalButton.BackColor = System.Drawing.Color.Transparent;
+            this.totalButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.totalButton.Location = new System.Drawing.Point(64, 364);
+            this.totalButton.Name = "totalButton";
+            this.totalButton.Size = new System.Drawing.Size(345, 49);
+            this.totalButton.TabIndex = 7;
+            this.totalButton.Text = "Calculate Total";
+            this.totalButton.UseVisualStyleBackColor = false;
+            // 
+            // subTotal
+            // 
+            this.subTotal.AutoSize = true;
+            this.subTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.subTotal.Location = new System.Drawing.Point(60, 447);
+            this.subTotal.Name = "subTotal";
+            this.subTotal.Size = new System.Drawing.Size(84, 25);
+            this.subTotal.TabIndex = 8;
+            this.subTotal.Text = "Subtotal";
+            this.subTotal.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // tax
+            // 
+            this.tax.AutoSize = true;
+            this.tax.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tax.Location = new System.Drawing.Point(60, 488);
+            this.tax.Name = "tax";
+            this.tax.Size = new System.Drawing.Size(46, 25);
+            this.tax.TabIndex = 9;
+            this.tax.Text = "Tax";
+            // 
+            // total
+            // 
+            this.total.AutoSize = true;
+            this.total.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.total.Location = new System.Drawing.Point(60, 529);
+            this.total.Name = "total";
+            this.total.Size = new System.Drawing.Size(56, 25);
+            this.total.TabIndex = 10;
+            this.total.Text = "Total";
+            // 
+            // tenderedLabel
+            // 
+            this.tenderedLabel.AutoSize = true;
+            this.tenderedLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tenderedLabel.Location = new System.Drawing.Point(60, 616);
+            this.tenderedLabel.Name = "tenderedLabel";
+            this.tenderedLabel.Size = new System.Drawing.Size(97, 25);
+            this.tenderedLabel.TabIndex = 11;
+            this.tenderedLabel.Text = "Tendered";
+            // 
+            // tenderBox
+            // 
+            this.tenderBox.Location = new System.Drawing.Point(309, 615);
+            this.tenderBox.Name = "tenderBox";
+            this.tenderBox.Size = new System.Drawing.Size(100, 26);
+            this.tenderBox.TabIndex = 12;
+            // 
+            // changeButton
+            // 
+            this.changeButton.BackColor = System.Drawing.Color.Transparent;
+            this.changeButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.changeButton.Location = new System.Drawing.Point(64, 671);
+            this.changeButton.Name = "changeButton";
+            this.changeButton.Size = new System.Drawing.Size(345, 41);
+            this.changeButton.TabIndex = 13;
+            this.changeButton.Text = "Calculate Change";
+            this.changeButton.UseVisualStyleBackColor = false;
+            // 
+            // changeLabel
+            // 
+            this.changeLabel.AutoSize = true;
+            this.changeLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.changeLabel.Location = new System.Drawing.Point(60, 735);
+            this.changeLabel.Name = "changeLabel";
+            this.changeLabel.Size = new System.Drawing.Size(82, 25);
+            this.changeLabel.TabIndex = 14;
+            this.changeLabel.Text = "Change";
+            // 
+            // receiptButton
+            // 
+            this.receiptButton.BackColor = System.Drawing.Color.Transparent;
+            this.receiptButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.receiptButton.Location = new System.Drawing.Point(64, 779);
+            this.receiptButton.Name = "receiptButton";
+            this.receiptButton.Size = new System.Drawing.Size(345, 41);
+            this.receiptButton.TabIndex = 15;
+            this.receiptButton.Text = "Print Receipt";
+            this.receiptButton.UseVisualStyleBackColor = false;
+            // 
+            // stPrice
+            // 
+            this.stPrice.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.stPrice.Location = new System.Drawing.Point(325, 447);
+            this.stPrice.Name = "stPrice";
+            this.stPrice.Size = new System.Drawing.Size(84, 25);
+            this.stPrice.TabIndex = 16;
+            // 
+            // taPrice
+            // 
+            this.taPrice.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.taPrice.Location = new System.Drawing.Point(325, 488);
+            this.taPrice.Name = "taPrice";
+            this.taPrice.Size = new System.Drawing.Size(84, 25);
+            this.taPrice.TabIndex = 17;
+            // 
+            // toPrice
+            // 
+            this.toPrice.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.toPrice.Location = new System.Drawing.Point(325, 529);
+            this.toPrice.Name = "toPrice";
+            this.toPrice.Size = new System.Drawing.Size(84, 25);
+            this.toPrice.TabIndex = 18;
+            // 
+            // newOrder
+            // 
+            this.newOrder.BackColor = System.Drawing.Color.Transparent;
+            this.newOrder.BackgroundImage = global::CashRegister.Properties.Resources.paddyssign3;
+            this.newOrder.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.newOrder.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.newOrder.Location = new System.Drawing.Point(737, 793);
+            this.newOrder.Name = "newOrder";
+            this.newOrder.Size = new System.Drawing.Size(335, 41);
+            this.newOrder.TabIndex = 19;
+            this.newOrder.Text = "New Order";
+            this.newOrder.UseVisualStyleBackColor = false;
+            // 
+            // caPrice
+            // 
+            this.caPrice.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.caPrice.Location = new System.Drawing.Point(325, 735);
+            this.caPrice.Name = "caPrice";
+            this.caPrice.Size = new System.Drawing.Size(84, 25);
+            this.caPrice.TabIndex = 21;
+            // 
+            // lineLabel
+            // 
+            this.lineLabel.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lineLabel.Location = new System.Drawing.Point(60, 580);
+            this.lineLabel.Name = "lineLabel";
+            this.lineLabel.Size = new System.Drawing.Size(349, 10);
+            this.lineLabel.TabIndex = 22;
+            this.lineLabel.Click += new System.EventHandler(this.label1_Click_2);
+            // 
+            // paddysSign
+            // 
+            this.paddysSign.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.paddysSign.Dock = System.Windows.Forms.DockStyle.Top;
+            this.paddysSign.Image = global::CashRegister.Properties.Resources.paddyssign3;
+            this.paddysSign.ImageLocation = "";
+            this.paddysSign.Location = new System.Drawing.Point(0, 0);
+            this.paddysSign.Name = "paddysSign";
+            this.paddysSign.Size = new System.Drawing.Size(1122, 83);
+            this.paddysSign.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.paddysSign.TabIndex = 23;
+            this.paddysSign.TabStop = false;
+            // 
+            // receiptPaper
+            // 
+            this.receiptPaper.Image = global::CashRegister.Properties.Resources.paper;
+            this.receiptPaper.Location = new System.Drawing.Point(737, 178);
+            this.receiptPaper.Name = "receiptPaper";
+            this.receiptPaper.Size = new System.Drawing.Size(335, 596);
+            this.receiptPaper.TabIndex = 1;
+            this.receiptPaper.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // menuPicture
+            // 
+            this.menuPicture.Image = global::CashRegister.Properties.Resources.menu;
+            this.menuPicture.Location = new System.Drawing.Point(29, 119);
+            this.menuPicture.Name = "menuPicture";
+            this.menuPicture.Size = new System.Drawing.Size(419, 722);
+            this.menuPicture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.menuPicture.TabIndex = 20;
+            this.menuPicture.TabStop = false;
+            // 
+            // pubBackground
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::CashRegister.Properties.Resources.paddy3;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1122, 861);
+            this.Controls.Add(this.paddysSign);
+            this.Controls.Add(this.lineLabel);
+            this.Controls.Add(this.caPrice);
+            this.Controls.Add(this.newOrder);
+            this.Controls.Add(this.toPrice);
+            this.Controls.Add(this.taPrice);
+            this.Controls.Add(this.stPrice);
+            this.Controls.Add(this.receiptButton);
+            this.Controls.Add(this.changeLabel);
+            this.Controls.Add(this.changeButton);
+            this.Controls.Add(this.tenderBox);
+            this.Controls.Add(this.tenderedLabel);
+            this.Controls.Add(this.total);
+            this.Controls.Add(this.tax);
+            this.Controls.Add(this.subTotal);
+            this.Controls.Add(this.totalButton);
+            this.Controls.Add(this.priceBox3);
+            this.Controls.Add(this.priceBox2);
+            this.Controls.Add(this.priceBox1);
+            this.Controls.Add(this.foodLabel3);
+            this.Controls.Add(this.foodLabel2);
+            this.Controls.Add(this.receiptPaper);
+            this.Controls.Add(this.foodLabel1);
+            this.Controls.Add(this.menuPicture);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "pubBackground";
+            this.Text = "Paddy\'s Pub";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.paddysSign)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.menuPicture)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label foodLabel1;
+        private System.Windows.Forms.Label receiptPaper;
+        private System.Windows.Forms.Label foodLabel2;
+        private System.Windows.Forms.Label foodLabel3;
+        private System.Windows.Forms.TextBox priceBox1;
+        private System.Windows.Forms.TextBox priceBox2;
+        private System.Windows.Forms.TextBox priceBox3;
+        private System.Windows.Forms.Button totalButton;
+        private System.Windows.Forms.Label subTotal;
+        private System.Windows.Forms.Label tax;
+        private System.Windows.Forms.Label total;
+        private System.Windows.Forms.Label tenderedLabel;
+        private System.Windows.Forms.TextBox tenderBox;
+        private System.Windows.Forms.Button changeButton;
+        private System.Windows.Forms.Label changeLabel;
+        private System.Windows.Forms.Button receiptButton;
+        private System.Windows.Forms.Label stPrice;
+        private System.Windows.Forms.Label taPrice;
+        private System.Windows.Forms.Label toPrice;
+        private System.Windows.Forms.Button newOrder;
+        private System.Windows.Forms.PictureBox menuPicture;
+        private System.Windows.Forms.Label caPrice;
+        private System.Windows.Forms.Label lineLabel;
+        private System.Windows.Forms.PictureBox paddysSign;
+    }
+}
+
---
+++ CashRegister/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CashRegister
+{
+    public partial class pubBackground : Form
+    {
+
+        public pubBackground()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+            double steak = 13.50;
+            int numOfSteak = 0;
+
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_2(object sender, EventArgs e)
+        {
+
+        }
+
+        private void foodLabel2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
